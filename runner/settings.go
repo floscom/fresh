8ecbc53daea2d26b3ae39a41875ddbf0927b3d33
Flo Narr <florian.narr@gmail.com>
2017-05-29 14:01:03 +0200

    added custom watch folder path

--- runner/settings.go
+++ runner/settings.go
@@ -20,6 +20,7 @@ const (
 var settings = map[string]string{
 	"config_path":       "./runner.conf",
 	"root":              ".",
+	"watch":             ".",
 	"tmp_path":          "./tmp",
 	"build_name":        "runner-build",
 	"build_log":         "runner-build-errors.log",
@@ -110,6 +111,10 @@ func getenv(key, defaultValue string) string {
 
 func root() string {
 	return settings["root"]
+}
+
+func watchFolderPath() string {
+	return settings["watch"]
 }
 
 func tmpPath() string {
